48c0be3c3431b1cf45dca01558a895c4c4d9d0dc
kvaga <32394407+kvaga@users.noreply.github.com>
2024-01-28 22:20:48 +0300

    test

--- colab/invest.py
+++ colab/invest.py
@@ -21,7 +21,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
  
-
+#1
 def getFullAmountOfInvestmentsInRub(\
                                     data, 
                                     usdrur, hkdrur, cnyrur, eurrur, 
